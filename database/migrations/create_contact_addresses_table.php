74c6c38a550069813b4e79f1c51a54459cc134e3
Phillip <pruhesi@gmail.com>
2023-02-03 14:41:38 +0200

    add contact addresses model and migration

---
+++ database/migrations/create_contact_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_addresses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('external_guid');
+            $table->string('line1');
+            $table->string('line2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('post_code')->nullable();
+            $table->string('country')->nullable();
+            $table->string('type');
+            $table->boolean('default_for_type')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_addresses');
+    }
+}
