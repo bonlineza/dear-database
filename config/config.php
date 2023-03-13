2a36b01f332ac58fd5a75f22f02520c2084fb3df
Phillip <pruhesi@gmail.com>
2023-03-13 15:43:18 +0200

    add address model and migration

--- config/config.php
+++ config/config.php
@@ -8,5 +8,6 @@ return [
         'contact' => \Bonlineza\DearDatabase\Models\Contact::class,
         'contact_address' => \Bonlineza\DearDatabase\Models\ContactAddress::class,
         'additional_attribute' => \Bonlineza\DearDatabase\Models\AdditionalAttribute::class,
+        'address' => \Bonlineza\DearDatabase\Models\Address::class,
     ]
 ];
