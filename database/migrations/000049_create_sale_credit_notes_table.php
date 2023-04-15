2e74896662f341217ad0d3eb4ddb113bfa6454d1
Phillip <pruhesi@gmail.com>
2023-04-15 14:30:37 +0200

    esnure external guid on sale and sale credit note  is a of type uuid

--- database/migrations/000049_create_sale_credit_notes_table.php
+++ database/migrations/000049_create_sale_credit_notes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('sale_credit_notes', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->text('external_guid')->nullable();
+            $table->uuid('external_guid')->nullable();
             $table->text('credit_note_invoice_number')->nullable();
             $table->text('memo')->nullable();
             $table->text('status');
