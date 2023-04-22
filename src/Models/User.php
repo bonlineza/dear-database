9936333a61c9eb35e6ff2490483ac8cd349cbe94
Phillip <pruhesi@gmail.com>
2023-04-22 17:52:15 +0200

    ensure user extends authenticable class
    this means when we extend this class in the specific project we also get the authenticable class instead of extending both

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,13 +7,13 @@ use Bonlineza\DearDatabase\Models\Contact;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
  * @property string $email
  */
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory, HasUuids;
 
